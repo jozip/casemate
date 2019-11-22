5caba46efb650977f6eeb1b6cf9c1702db7e44f8
Johan Persson <johan@2130.se>
2019-11-22 19:50:42 +0100

    Added missing build dependency (srfi-26)

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "casemate")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "srfi-lib"))
 (define scribblings '(("casemate.scrbl" ())))
 (define pkg-desc "Case converter in the style of camel-snake-kebab")
 (define version "1.0.0")
