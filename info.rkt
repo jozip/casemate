8a2a3801300b538f3152cd3829c2a19c996fd57e
Johan Persson <johan@2130.se>
2019-11-24 19:14:24 +0100

    v1.1.0

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("casemate.scrbl" ())))
 (define pkg-desc "Case converter in the style of camel-snake-kebab")
-(define version "1.0.0")
+(define version "1.1.0")
 (define pkg-authors '(jzp))
